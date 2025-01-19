475c0357b09d11ec7a7c1f86a0941317a8cc9d17
LamFoxer <dakfor@yandex.ru>
2025-01-19 22:46:46 +0300

    final2

--- 20.01/javascripts/java-script.js
+++ 20.01/javascripts/java-script.js
@@ -1,15 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
+'use strict';
+document.addEventListener("DOMContentLoaded", function(){
+    //всё выводится в консоль, чтобы не плодить уведы!
 
-    let myFunction = function() { // Тело функции
-        let integriruemtext = document.getElementById('integriruemtext');
+    //1
+    let str = 'abcde';
+    console.log(str[0]);
+    console.log(str[1]);
+    console.log(str[4]);
 
-        integriruemtext.textContent = "Hi student!"; // Добавили текст в див
+    //2
+    let num1 = prompt("Enter first number.", "");
+    let num2 = prompt("Enter second number.", "");
+    let result;
 
-        integriruemtext.style.fontFamily = 'CarlaSansLight'; // добавили стили к тексту 
-        integriruemtext.style.fontSize = "35px"; // добавили стили к тексту 
-    }; 
-    
-    myFunction(); // Вызов функции
-    
-    
-});
+    if (num1 > 0) {
+        result = true;
+    }
+    else if (num2 > 0) {
+        result = true;
+    }
+    else {
+        result = false;
+    }
+    console.log(result);
+
+    //3
+    let userAge = parseInt(prompt("Enter your age (e.g. 18, 37).", ""));
+    if (userAge >= 18) {
+        console.log("Совершеннолетний");
+    }
+    else if (userAge < 18) {
+        console.log("Несовершеннолетний");
+    }
+    else {
+        console.log("error");
+    }
+})
