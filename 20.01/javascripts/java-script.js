d2fe2f64df1bb673510b499bf8971b0b76d914e5
LamFoxer <dakfor@yandex.ru>
2025-01-27 11:46:36 +0300

    final4

--- 20.01/javascripts/java-script.js
+++ 20.01/javascripts/java-script.js
@@ -1,5 +1,10 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", function(){
+    /* 
+|||||||||||||||||||||||||||||||||||||||||||
+||||   ДОМАШНЯЯ РАБОТА 1 (ЗА 20.01)   |||||
+|||||||||||||||||||||||||||||||||||||||||||
+
     //всё выводится в консоль, чтобы не плодить уведы!
 
     //1
@@ -35,4 +40,62 @@ document.addEventListener("DOMContentLoaded", function(){
     else {
         console.log("error");
     }
+    */
+
+    /* 
+    |||||||||||||||||||||||||||||||||||||||||||
+    ||||   ДОМАШНЯЯ РАБОТА 2 (ЗА 27.01)   |||||
+    |||||||||||||||||||||||||||||||||||||||||||
+    */
+    //задача 1
+
+    let numberOne = parseInt(prompt("Введите число арабскими цифрами:", ""));
+
+    let numberTwo = parseInt(prompt("Введите число арабскими цифрами:", ""));
+    
+    if (numberOne > numberTwo) {
+        alert(numberOne + " больше " + numberTwo);
+    }
+    
+    else if(numberOne < numberTwo) {
+        alert(numberOne + " меньше " + numberTwo);
+    }
+    
+    else if(numberOne === numberTwo) {
+        alert(numberOne + " равно " + numberTwo);
+    }
+    
+    else {     
+        alert("Ошибка! Введите два арабских часла (пример: 32, 2, 67,4)");
+    }
+    //конец задачи 1
+    //задача 2
+    let numberThree = parseInt(prompt("Введите число арабскими цифрами для проверки на чётность:", ""));
+
+    if( (numberThree % 2) === 0) {
+        alert(numberThree + " - чётное");
+        console.log("1");
+    }
+    
+    else if( (numberThree % 2) != 0 ) {
+        alert(numberThree + " - нечётное");
+    }
+
+    else {     
+        alert("Ошибка! Введите арабское число (пример: 32, 2, 67,4)");
+    }
+    //конец задачи 2
+    //задача 3
+    const secretNumber = 2;
+    let numberFour = parseInt(prompt("Угадайте, какое число от 0 до 10 я загадал?", ""));
+
+    if ( numberFour === secretNumber) {
+        alert(numberFour + " Угадал!");
+    }
+
+    else {     
+        alert("Не получилось.");
+    }
+    //конец задачи 3
+
 })
